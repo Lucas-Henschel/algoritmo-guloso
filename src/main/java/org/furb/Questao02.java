0d4677595e7cb81d77e94274e1a3b9b91006e208
LucasHenschel <lucas101henschel@gmail.com>
2026-05-29 21:11:14 -0300

    first commit

---
+++ src/main/java/org/furb/Questao02.java
@@ -0,0 +1,7 @@
+package org.furb;
+
+public class Questao02 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/org/furb/Questao03.java
@@ -0,0 +1,7 @@
+package org.furb;
+
+public class Questao03 {
+    public static void main(String[] args) {
+        
+    }
+}
